c6880fc8116d995b135aabe004a8e5a19b6a9e17
codeTIT4N <lokeshkumar636@yahoo.com>
2022-03-06 16:22:18 +0530

    fetching balance using web3 crate and infura

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-# Rust Web3 crate example
+# Basic Rust Web3 example
 
-A simple implementation for web3 crate of rust.
+A simple implementation for web3 crate of rust. It query the blockchain node and gets you your approx. ether balance.
 
-To run:
+Reference: https://tms-dev-blog.com/rust-web3-connect-to-ethereum/
+
+To run the project:
+
+Step 1: Make sure you have cargo and rust installed in your system
+
+Step 2: Rename the .env.example to .env
+
+Step 3: Insert your Infura Key for your network (Mainnet, Rinkeby, Ropsten, etc) and your ethereum account address in the .env file
+
+Step 4: Run the project using
 ```
 cargo run
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::env; //for environment variables
+use std::str::FromStr; //trait for using Address::from_str
+
+use web3::types::{H160, U256}; //for processing / instancing smart contract addresses and handling return values.
+
+#[tokio::main]
+async fn main() -> web3::Result<()> {
+    dotenv::dotenv().ok();
+
+    //instance to establish connection with ethereum network
+    let websocket = web3::transports::WebSocket::new(&env::var("INFURA_WSS").unwrap()).await?;
+    //web3 instance
+    let web3s = web3::Web3::new(websocket);
+    //getting accounts
+    let mut accounts = web3s.eth().accounts().await?;
+    //adding our account to the array
+    accounts.push(H160::from_str(&env::var("ACCOUNT_ADDRESS").unwrap()).unwrap());
+    //to convert the wei balance to ether balance
+    let wei_conv: U256 = U256::exp10(18);
+    //for printing balance of all the accounts in the array
+    for account in accounts {
+        //getting the balance in wei from web3 instance
+        let balance = web3s.eth().balance(account, None).await?;
+        println!("Wei balance of {:?}: {:?}", account, balance);
+        println!(
+            "ETH balance of {:?}: {}",
+            account,
+            balance.checked_div(wei_conv).unwrap()
+        );
+    }
+    Ok(())
 }
